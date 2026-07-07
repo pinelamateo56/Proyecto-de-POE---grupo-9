cc2f0582a5d9131ead6b5ee81bbf56a756826c12
pinelamateo56 <pinelamateo56@gmail.com>
2026-07-06 22:44:13 -0500

    Add files via upload

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,12 +4,13 @@ const controller = require('../controllers/authController');
 const authMiddleware = require('../middleware/authMiddleware');
 const roleMiddleware = require('../middleware/roleMiddleware');
 
-router.post('/register/public', controller.register);
-
 router.post('/login', controller.login);
 router.post('/register', authMiddleware, roleMiddleware('admin'), controller.register);
+router.post('/register/public', controller.register);
 router.get('/perfil', authMiddleware, controller.obtenerPerfil);
 router.get('/', authMiddleware, roleMiddleware('admin'), controller.listar);
+router.get('/inactivos', authMiddleware, roleMiddleware('admin'), controller.listarInactivos);
+router.put('/:id/reactivar', authMiddleware, roleMiddleware('admin'), controller.reactivar);
 router.delete('/:id', authMiddleware, roleMiddleware('admin'), controller.eliminar);
 
 module.exports = router;
