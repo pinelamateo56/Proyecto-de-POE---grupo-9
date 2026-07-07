5ed241b05190e70409cc76b6b6aef74cd4fb2422
pinelamateo56 <pinelamateo56@gmail.com>
2026-07-06 21:23:49 -0500

    Add public registration route to authRoutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,6 +4,8 @@ const controller = require('../controllers/authController');
 const authMiddleware = require('../middleware/authMiddleware');
 const roleMiddleware = require('../middleware/roleMiddleware');
 
+router.post('/register/public', controller.register);
+
 router.post('/login', controller.login);
 router.post('/register', authMiddleware, roleMiddleware('admin'), controller.register);
 router.get('/perfil', authMiddleware, controller.obtenerPerfil);
